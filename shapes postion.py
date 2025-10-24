7ca97e6bfc7e5d7ed589080187df9f3f000849ea
Megan Loe <meganhelenaloe@gmail.com>
2025-10-24 11:15:16 +0200

    the starting base of code + UML

---
+++ shapes postion.py
@@ -0,0 +1,16 @@
+#this is going to be the parent class for the shapes
+class ShapesPosition:
+    #I'm basing this off of the OldCoinStash example from kokchun
+    def __init__(self, shape):
+        self.shape = shape
+        # this is the starting position of the shapes
+        self._x = 0
+        self._y = 0
+    
+    def postion(self, x, y): #for now its going to avoid the negative positions
+        if x <= 0 or y <= 0:
+            raise ValueError(
+                f"you can't place X at {x} position or Y at {y} position, they need to be above 0")
+        
+        self._x += x
+        self._y += y
