17e1b163741825b72851e6ea116f7bc50005dc81
Megan Loe <meganhelenaloe@gmail.com>
2025-10-30 13:06:50 +0100

    changin name on test file + finishing touches on the parent class

--- shapes_position.py
+++ shapes_position.py
@@ -1,31 +1,24 @@
-from numbers import Number
+from numbers import Number #x and y need their numbers
 
 #this is going to be the parent class for the shapes
 class ShapesPosition:
     #I'm basing this off of the OldCoinStash example from kokchun
+    #this is the starting position of the shapes
     def __init__(self, x, y):
         if not isinstance(x, Number):
             raise TypeError(f"please put in a number not {x}")
-
+        #this is the validation area for x and y
         if not isinstance(y, Number):
             raise TypeError(f"please put in a number not {y}")
-        
+        #made x and y read only, becuase you don't need to see the starting position
         self._x = x
         self._y = y
-
 
     @property
     def x(self):
         return self._x
-
+    #these return x and y, more specifically the changed version of them
     @property
     def y(self):
         return self._y
 
-        #this is the starting position of the shapes
-
-#trying to follow the inheritance example from kokchun
-class Circle(ShapesPosition):
-
-    def __init__(self, x, y):
-        super().__init__(x, y)
